26c3b9585c17ef96d81f3f3a05a54b66302ce80b
gundenaf <gundenaf@gmail.com>
2019-03-15 16:48:37 +0300

    Add some tasks from Chapter 4

--- Chapter 4/word_jumble.py
+++ Chapter 4/word_jumble.py
@@ -3,9 +3,23 @@
 #   Задача игрока - восстановить исходное слово
 import random
 
+WORDS = ("питон", "анаграмма", "простая", "сложная", "ответ", "подстаканник")
+word = random.choice(WORDS)
+correct = word
+jumble = ""
+while word:
+    position = random.randrange(len(word))
+    jumble += word[position]
+    word = word[:position] + word[(position + 1):]
 print("""Добро пожаловать в игру 'Анаграммы!'
 Надо переставить буквы так, чтобы получилось осмысленное слово.
 (Для выхода нажмите Enter, не вводя своей версии.)\n""")
-jumble: str = "s"
-print("Вот анаграмма: %s\n" % jumble)
-guess = str(input("Попробуйте отгадать исходное слово: "))
+print("Вот анаграмма:", jumble)
+guess = input("\nПопробуйте отгадать исходное слово: ")
+while guess != correct and guess != "":
+    print("К сожалению, вы неправы.")
+    guess = input("Попробуйте отгадать исходное слово: ")
+if guess == correct:
+    print("Да, именно так! Вы отгадали!\n")
+print("Спасибо за игру.")
+input("\n\nНажмите Enter, чтобы выйти.")
